2be8a1364c83edc81b59c27ed9cb4a1f5d3b5108
Kyle Pacheco <kpacheco@nnu.edu>
2021-03-18 20:11:59 -0600

    Finishing looping input

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,13 +1,7 @@
 import org.junit.Test;
 
 public class MainTest {
-
-    @Test
-    public void main_alphaInput() {
-        Main driver = new Main();
-        String[] input = {"1", "abc"};
-        driver.main(input);
-    }
+    
     @Test
     public void main_standardInput() {
         Main driver = new Main();
--- src/UserPrompt.java
+++ src/UserPrompt.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class UserPrompt {
 
     public String[] prompt() {
-        String[] input = {};
+        String[] input = new String[2];
         input[0] = promptUser("Enter first bound:");
         input[1] = promptUser("Enter second bound:");
         return input;
